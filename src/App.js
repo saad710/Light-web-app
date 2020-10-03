2b50ec2ce0f01df26e46f06c691f4cbfd7fa328b
Sajeeb <sajeebahamed6@gmail.com>
2020-10-03 12:54:07 +0600

    react-router added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Dashboard from './components/MainDashboard/Dashboard';
 import SignInSide from './components/SignIn/SignIn';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <SignInSide />
-       {/* <Dashboard /> */}
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <SignInSide />
+          </Route>
+          <Route path="/inbox">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -7,6 +7,7 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import {NavLink} from 'react-router-dom'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -103,7 +104,9 @@ export default function SignInSide() {
                             color="primary"
                             className={classes.submit}
                         >
-                            Sign In
+                            <NavLink to="/inbox">
+                                Sign In
+                            </NavLink>
                         </Button>
                         <Grid container>
                             <Grid item xs>
