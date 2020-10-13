54c9c2fad40b7582c156b35aef92e9b15b7194e7
Sajeeb <sajeebahamed6@gmail.com>
2020-10-13 15:25:04 +0600

    email signature added

--- src/components/MainDashboard/EmailSignature/EmailSignature.js
+++ src/components/MainDashboard/EmailSignature/EmailSignature.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
@@ -10,6 +10,13 @@ import TwitterIcon from '@material-ui/icons/Twitter';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import LanguageIcon from '@material-ui/icons/Language';
 import { Button } from '@material-ui/core';
+import SunEditor from 'suneditor-react';
+import 'suneditor/dist/css/suneditor.min.css';
+import '../Coompose/Compose.css'
+import CancelOutlinedIcon from '@material-ui/icons/CancelOutlined';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -61,11 +68,47 @@ const useStyles = makeStyles((theme) => ({
         padding: '0.3rem 1.5rem',
         border: '1px solid gray',
         margin: '0 1rem 2rem 1rem'
+    },
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: '12rem',
+        
+    },
+    emailModal : {
+        // backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+        backgroundColor: '#213F7E'
+    },
+    signatureBtn : {
+        padding: '0.3rem 2rem',
+        marginLeft: '40rem'
     }
 }));
 
 const EmailSignature = () => {
     const classes = useStyles();
+    const [addEmailModal, setAddEmailModal] = useState(false);
+    const [updateEmailModal, setUpdateEmailModal] = useState(false);
+
+    const handleAddEmailModalOpen = () => {
+        setAddEmailModal(true);
+    };
+
+    const handleAddEmailModalClose = () => {
+        setAddEmailModal(false);
+    };
+
+    const handleUpdateEmailModalOpen = () => {
+        setUpdateEmailModal(true);
+    };
+
+    const handleUpdateEmailModalClose = () => {
+        setUpdateEmailModal(false);
+    };
     return (
         <div className={classes.root}>
             <CssBaseline />
@@ -75,11 +118,11 @@ const EmailSignature = () => {
                 <Container maxWidth="md" className={classes.container}>
                     <Grid container spacing={3}>
                         <div className={classes.paper}>
-                            <div style={{margin: '0 auto'}}>
-                                <Button variant="contained" className={classes.emailBtn}>
+                            <div style={{ margin: '0 auto' }}>
+                                <Button onClick={handleAddEmailModalOpen} variant="contained" className={classes.emailBtn}>
                                     Add New
                                 </Button>
-                                <Button variant="contained" className={classes.emailBtn}>
+                                <Button onClick={handleUpdateEmailModalOpen} variant="contained" className={classes.emailBtn}>
                                     Update
                                 </Button>
                             </div>
@@ -130,6 +173,82 @@ const EmailSignature = () => {
                             </div>
 
                         </div>
+
+                        <div>
+                            {/* Add email Signature start */}
+                            <Modal
+                                aria-labelledby="transition-modal-title"
+                                aria-describedby="transition-modal-description"
+                                className={classes.modal}
+                                open={addEmailModal}
+                                onClose={handleAddEmailModalClose}
+                                closeAfterTransition
+                                BackdropComponent={Backdrop}
+                                BackdropProps={{
+                                    timeout: 500,
+                                }}
+                            >
+                                <Fade in={addEmailModal}>
+                                    <div className={classes.emailModal}>
+                                        <h6 className="text-center" style={{color: '#fff', padding: '0.5rem 0'}}> Add new Signature </h6>
+                                    <SunEditor
+                                            width="100  %"
+                                            placeholder=" Email Signature "
+                                            setOptions={{
+                                                height: 150,
+                                                buttonList: [
+                                                    ['font', 'fontSize', 'formatBlock'],
+                                                    ['bold', 'underline', 'italic',],
+                                                    ['align', 'horizontalRule', 'list', 'lineHeight'],
+                                                    ['link', 'image', 'video',],
+                                                    ['fullScreen', 'codeView'],
+                                                ]
+                                            }}
+                                        />
+                                        <Button className={classes.signatureBtn} variant="contained"> Add </Button>
+                                    </div>
+                                </Fade>
+                            </Modal>
+                            {/* Add email Signature end */}
+
+
+                            {/* Update email Signature start */}
+                            <Modal
+                                aria-labelledby="transition-modal-title"
+                                aria-describedby="transition-modal-description"
+                                className={classes.modal}
+                                open={updateEmailModal}
+                                onClose={handleUpdateEmailModalClose}
+                                closeAfterTransition
+                                BackdropComponent={Backdrop}
+                                BackdropProps={{
+                                    timeout: 500,
+                                }}
+                            >
+                                <Fade in={updateEmailModal}>
+                                    <div className={classes.emailModal}>
+                                        <h6 className="text-center" style={{ color: '#fff', padding: '0.5rem 0' }}> Update Signature </h6>
+                                        <SunEditor
+                                            width="100  %"
+                                            placeholder=" Email Signature "
+                                            setOptions={{
+                                                height: 150,
+                                                buttonList: [
+                                                    ['font', 'fontSize', 'formatBlock'],
+                                                    ['bold', 'underline', 'italic',],
+                                                    ['align', 'horizontalRule', 'list', 'lineHeight'],
+                                                    ['link', 'image', 'video',],
+                                                    ['fullScreen', 'codeView'],
+                                                ]
+                                            }}
+                                        />
+                                        <Button className={classes.signatureBtn} variant="contained"> Update </Button>
+                                    </div>
+                                </Fade>
+                            </Modal>
+                            {/* Update email Signature end */}
+                        </div>
+
                     </Grid>
 
                 </Container>
