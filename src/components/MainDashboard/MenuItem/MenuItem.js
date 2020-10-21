6bd6db14d7e532afb45c1eafc91c9378a84dbbc2
Sajeeb <sajeebahamed6@gmail.com>
2020-10-21 18:13:10 +0600

    fixed the icon hover issue

--- src/components/MainDashboard/MenuItem/MenuItem.js
+++ src/components/MainDashboard/MenuItem/MenuItem.js
@@ -57,8 +57,6 @@ const MenuItem = () => {
   const [ticketModal, setTicketModal] = React.useState(false);
   const [ticketValue, setTicketValue] = useState({});
 
-  console.log(ticketValue);
-
   const handleClick = () => {
     setOpen(!open);
   };
@@ -80,7 +78,24 @@ const MenuItem = () => {
     console.log(finalValue);
     e.preventDefault();
   };
-  const [isShown3, setIsShow3] = useState(false);
+
+  // icon hover state start
+  const [icon1, setIcon1] = useState(false);
+  const [icon2, setIcon2] = useState(false);
+  const [icon3, setIcon3] = useState(false);
+  const [icon4, setIcon4] = useState(false);
+  const [icon5, setIcon5] = useState(false);
+  const [icon6, setIcon6] = useState(false);
+  const [icon7, setIcon7] = useState(false);
+  const [icon8, setIcon8] = useState(false);
+  const [icon9, setIcon9] = useState(false);
+  const [icon10, setIcon10] = useState(false);
+  const [icon11, setIcon11] = useState(false);
+  const [icon12, setIcon12] = useState(false);
+  const [icon13, setIcon13] = useState(false);
+  const [icon14, setIcon14] = useState(false);
+  const [icon15, setIcon15] = useState(false);
+  // icon hover state end
   return (
     <div>
       <div>
@@ -96,73 +111,95 @@ const MenuItem = () => {
         </Link>
 
         <Link to='/dashboard'>
-          <ListItem button className={classes.listItem}>
+          <ListItem button className={classes.listItem}
+            onMouseEnter={() => setIcon1(true)}
+            onMouseLeave={() => setIcon1(false)}
+          >
             <ListItemIcon className={classes.iconStyle}>
-              <DashboardIcon/>
+              {icon1 ? <DashboardIcon style={{ color: '#fff' }} /> : <DashboardIcon />}
             </ListItemIcon>
             <ListItemText primary='Dashboard' />
           </ListItem>
         </Link>
 
         <Link to='/customers'>
-          <div className={classes.iconC}>
-            <ListItem button className={classes.listItem}>
-              <ListItemIcon className={classes.iconStyle}>
-                <WcIcon />
-              </ListItemIcon>
-              <ListItemText primary='Customers' />
-            </ListItem>
-          </div>
+          <ListItem button className={classes.listItem}
+            onMouseEnter={() => setIcon2(true)}
+            onMouseLeave={() => setIcon2(false)}
+          >
+            <ListItemIcon className={classes.iconStyle}>
+              {icon2 ? <WcIcon style={{ color: '#fff' }} /> : <WcIcon />}
+            </ListItemIcon>
+            <ListItemText primary='Customers' />
+          </ListItem>
         </Link>
 
         <Link to='/inbox'>
-          <ListItem button className={classes.listItem}>
+          <ListItem button className={classes.listItem}
+            onMouseEnter={() => setIcon3(true)}
+            onMouseLeave={() => setIcon3(false)}
+          >
             <ListItemIcon className={classes.iconStyle}>
-              <InboxIcon />
+              {icon3 ? <InboxIcon style={{ color: '#fff' }} /> : <InboxIcon />}
             </ListItemIcon>
             <ListItemText primary='Inbox' />
           </ListItem>
         </Link>
 
         <Link to='/sent'>
-          <ListItem button className={classes.listItem}>
+          <ListItem button className={classes.listItem}
+            onMouseEnter={() => setIcon4(true)}
+            onMouseLeave={() => setIcon4(false)}
+          >
             <ListItemIcon className={classes.iconStyle}>
-              <SendIcon />
+              {icon4 ? <SendIcon style={{ color: '#fff' }} /> : <SendIcon />}
             </ListItemIcon>
             <ListItemText primary='Sent' />
           </ListItem>
         </Link>
 
         <Link to="/group">
-          <ListItem button className={classes.listItem}>
+          <ListItem button className={classes.listItem}
+            onMouseEnter={() => setIcon5(true)}
+            onMouseLeave={() => setIcon5(false)}
+          >
             <ListItemIcon className={classes.iconStyle}>
-              <GroupWorkIcon />
+              {icon5 ? <GroupWorkIcon style={{ color: '#fff' }} /> : <GroupWorkIcon />}
             </ListItemIcon>
             <ListItemText primary='Group' />
           </ListItem>
         </Link>
 
         <Link to='/tag'>
-          <ListItem button className={classes.listItem}>
+          <ListItem button className={classes.listItem}
+            onMouseEnter={() => setIcon6(true)}
+            onMouseLeave={() => setIcon6(false)}
+          >
             <ListItemIcon className={classes.iconStyle}>
-              <LabelOffOutlinedIcon />
+              {icon6 ? <LabelOffOutlinedIcon style={{ color: '#fff' }} /> : <LabelOffOutlinedIcon />}
             </ListItemIcon>
             <ListItemText primary='Tag' />
           </ListItem>
         </Link>
 
         <Link to='/calender'>
-          <ListItem button className={classes.listItem}>
+          <ListItem button className={classes.listItem}
+            onMouseEnter={() => setIcon7(true)}
+            onMouseLeave={() => setIcon7(false)}
+          >
             <ListItemIcon className={classes.iconStyle}>
-              <DateRangeOutlinedIcon />
+              {icon7 ? <DateRangeOutlinedIcon style={{ color: '#fff' }} /> : <DateRangeOutlinedIcon />}
             </ListItemIcon>
             <ListItemText primary='Calender' />
           </ListItem>
         </Link>
 
-        <ListItem button onClick={handleClick} className={classes.listItem}>
+        <ListItem button onClick={handleClick} className={classes.listItem}
+          onMouseEnter={() => setIcon8(true)}
+          onMouseLeave={() => setIcon8(false)}
+        >
           <ListItemIcon className={classes.iconStyle}>
-            <SettingsIcon />
+            {icon8 ? <SettingsIcon style={{ color: '#fff' }} /> : <SettingsIcon />}
           </ListItemIcon>
           <ListItemText primary='Setting' />
           {open ? <ExpandLess /> : <ExpandMore />}
@@ -176,20 +213,28 @@ const MenuItem = () => {
         >
           <List component='div' disablePadding>
             <Link to='/profile'>
-              <ListItem button className={classes.listItem}>
+              <ListItem button className={classes.listItem}
+                onMouseEnter={() => setIcon9(true)}
+                onMouseLeave={() => setIcon9(false)}
+              >
                 <div className="d-flex" style={{ marginLeft: '1rem' }}>
                   <ListItemIcon className={classes.iconStyle}>
-                    <PersonIcon />
+                    {icon9 ? <PersonIcon style={{ color: '#fff' }} /> : <PersonIcon />}
+                    {/* <PersonIcon /> */}
                   </ListItemIcon>
                   <ListItemText primary='Profile' />
                 </div>
               </ListItem>
             </Link>
             <Link to='/privacy'>
-              <ListItem button className={classes.listItem}>
+              <ListItem button className={classes.listItem}
+                onMouseEnter={() => setIcon10(true)}
+                onMouseLeave={() => setIcon10(false)}
+              >
                 <div className="d-flex" style={{ marginLeft: '1rem' }}>
                   <ListItemIcon className={classes.iconStyle}>
-                    <HttpsIcon />
+                    {icon10 ? <HttpsIcon style={{ color: '#fff' }} /> : <HttpsIcon />}
+                    {/* <HttpsIcon /> */}
                   </ListItemIcon>
                   <ListItemText primary='Privacy' />
                 </div>
@@ -197,10 +242,14 @@ const MenuItem = () => {
             </Link>
 
             <Link to='/team'>
-              <ListItem button className={classes.listItem}>
+              <ListItem button className={classes.listItem}
+                onMouseEnter={() => setIcon11(true)}
+                onMouseLeave={() => setIcon11(false)}
+              >
                 <div className="d-flex" style={{ marginLeft: '1rem' }}>
                   <ListItemIcon className={classes.iconStyle}>
-                    <GroupWorkIcon />
+                    {icon11 ? <SettingsIcon style={{ color: '#fff' }} /> : <SettingsIcon />}
+                    {/* <GroupWorkIcon /> */}
                   </ListItemIcon>
                   <ListItemText primary='Team' />
 
@@ -209,23 +258,31 @@ const MenuItem = () => {
             </Link>
 
             <Link to='/email-signature'>
-              <ListItem button className={classes.listItem}>
+              <ListItem button className={classes.listItem}
+                onMouseEnter={() => setIcon12(true)}
+                onMouseLeave={() => setIcon12(false)}
+              >
                 <div className="d-flex" style={{ marginLeft: '1rem' }}>
-                <ListItemIcon className={classes.iconStyle}>
-                  <LabelImportantIcon />
-                </ListItemIcon>
-                <ListItemText primary='Email Signature' />
+                  <ListItemIcon className={classes.iconStyle}>
+                    {icon12 ? <LabelImportantIcon style={{ color: '#fff' }} /> : <LabelImportantIcon />}
+                    {/* <LabelImportantIcon /> */}
+                  </ListItemIcon>
+                  <ListItemText primary='Email Signature' />
 
                 </div>
               </ListItem>
             </Link>
 
             <Link to='/companydetails'>
-              <ListItem button className={classes.listItem}>
+              <ListItem button className={classes.listItem}
+                onMouseEnter={() => setIcon13(true)}
+                onMouseLeave={() => setIcon13(false)}
+              >
                 <div className="d-flex" style={{ marginLeft: '1rem' }}>
-                <ListItemIcon className={classes.iconStyle}>
-                  <BusinessIcon />
-                </ListItemIcon>
+                  <ListItemIcon className={classes.iconStyle}>
+                    {icon13 ? <BusinessIcon style={{ color: '#fff' }} /> : <BusinessIcon />}
+                    {/* <BusinessIcon /> */}
+                  </ListItemIcon>
 
                 </div>
                 <ListItemText primary='Company Details' />
@@ -234,19 +291,27 @@ const MenuItem = () => {
           </List>
         </Collapse>
 
-        <ListItem onClick={handleOpen} button className={classes.listItem}>
+        <ListItem onClick={handleOpen} button className={classes.listItem}
+          onMouseEnter={() => setIcon14(true)}
+          onMouseLeave={() => setIcon14(false)}
+        >
           <div className='d-flex'>
             <ListItemIcon className={classes.iconStyle}>
-              <BugReportIcon />
+              {icon14 ? <BugReportIcon style={{ color: '#fff' }} /> : <BugReportIcon />}
+              {/* <BugReportIcon /> */}
             </ListItemIcon>
             <ListItemText primary='Ticket' />
           </div>
         </ListItem>
 
         <Link to='/'>
-          <ListItem button className={classes.listItem}>
+          <ListItem button className={classes.listItem}
+            onMouseEnter={() => setIcon15(true)}
+            onMouseLeave={() => setIcon15(false)}
+          >
             <ListItemIcon className={classes.iconStyle}>
-              <ExitToAppIcon />
+              {icon15 ? <ExitToAppIcon style={{ color: '#fff' }} /> : <ExitToAppIcon />}
+              {/* <ExitToAppIcon /> */}
             </ListItemIcon>
             <ListItemText primary='Log Out' />
           </ListItem>
